a7b5f58164d019a8980f9cb944d641ee4d585d77
Mohd Rejoan <wearecrx@gmail.com>
2022-12-26 01:31:35 +0600

    smooth value created

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
 $("#btn").click(function () {
+    // user input
     const userInput = $("#user-value").val();
+    // percentage calculator
+    function per(high, low, input) {
+        return (high - low) * (input / 100);
+    }
+    // c value h = 169 , l = 98
+    const cValue = per(169, 98, 100 - userInput);
+    const curveValue = per(248, 50, 100 - userInput);
+    const alterValue = per(400, 320, 100 - userInput); /// genrate minus val
+
+
     // value generator
-    let alterInput = 50 - userInput;
-    let highest = 250;
-    let totalDifferent = 170;
-    let lowest = 20;
-    if (!(userInput < 0 || userInput > 50)) {
-        const pathValue = 170 * (alterInput / 100) * 2;
+    // let alterInput = 100 - userInput;
+    // let alterInput = userInput;
+    // let highest = 250;
+    // let lowest = 60;
+
+    if (!(userInput < 0 || userInput > 100)) {
+        // const pathValue = (highest - lowest) * (alterInput / 100);
         // generate path
         $("#svg path").attr(
             "d",
-            `M100,250 C100,${pathValue + lowest} 399,${
-                pathValue + lowest
+            `M100,250 C${cValue + 98},${curveValue + 50} ${400 - alterValue},${
+                curveValue + 50
             } 400,250`
         );
+        // $("#svg path").attr(
+        //     "d",
+        //     `M100,250 C100,${pathValue + lowest} 399,${
+        //         pathValue + lowest
+        //     } 400,250`
+        // );
     } else {
-        alert('select a input value between 0-50')
+        alert("select a input value between 0-50");
     }
 });
