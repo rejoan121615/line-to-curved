0ce790037fef583e4158bdfe7ff1a4c65506d6df
Mohd Rejoan <wearecrx@gmail.com>
2022-12-25 21:16:11 +0600

    stable curved

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 $("#btn").click(function () {
     const userInput = $("#user-value").val();
     // value generator
-    let alterInput = (50 - userInput);
+    let alterInput = 50 - userInput;
     let highest = 250;
-    let lowest = 80;
     let totalDifferent = 170;
-    const pathValue = 170 * (alterInput / 100) * 2;
-    console.log(pathValue + lowest);
-    // generate path
-    $("#svg path").attr(
-        "d",
-        `M100,250 C100,${pathValue + lowest} 399,${pathValue + lowest} 400,250`
-    );
+    let lowest = 20;
+    if (!(userInput < 0 || userInput > 50)) {
+        const pathValue = 170 * (alterInput / 100) * 2;
+        // generate path
+        $("#svg path").attr(
+            "d",
+            `M100,250 C100,${pathValue + lowest} 399,${
+                pathValue + lowest
+            } 400,250`
+        );
+    } else {
+        alert('select a input value between 0-50')
+    }
 });
