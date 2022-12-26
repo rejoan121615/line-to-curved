469f0b4919e4a2eda5e31df4857a80a88bed3253
Mohd Rejoan <wearecrx@gmail.com>
2022-12-26 12:23:39 +0600

    css transitation effect added

--- js/script.js
+++ js/script.js
@@ -11,15 +11,27 @@ $("#btn").click(function () {
     // const alterValue = per(400, 320, 100 - userInput);
     // generate path
     if (!(userInput < 0 || userInput > 100)) {
-        const cValue = per(208, 100, 100 - userInput);
-        const curveValue = per(188, 50, 100 - userInput);
-        const alterValue = per(400, 290, 100 - userInput);
-        $("#svg path").attr(
-            "d",
-            `M100,250 C${cValue + 100},${curveValue + 50} ${400 - alterValue},${
-                curveValue + 50
-            } 400,250`
-        );
+        if (!(userInput < 30 || userInput > 100)) {
+            const cValue = per(208, 100, 100 - userInput);
+            const curveValue = per(188, 50, 100 - userInput);
+            const alterValue = per(400, 290, 100 - userInput);
+            $("#svg path").attr(
+                "d",
+                `M100,250 C${cValue + 100},${curveValue + 50} ${
+                    400 - alterValue
+                },${curveValue + 50} 400,250`
+            );
+        } else {
+            const cValue = per(169, 100, 100 - userInput);
+            const curveValue = per(250, 50, 100 - userInput);
+            const alterValue = per(400, 320, 100 - userInput);
+            $("#svg path").attr(
+                "d",
+                `M100,250 C${cValue + 100},${curveValue + 50} ${
+                    400 - alterValue
+                },${curveValue + 50} 400,250`
+            );
+        }
     } else {
         alert("select a input value between 0-50");
     }
